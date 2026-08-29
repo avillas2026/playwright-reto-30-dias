d62692dcc152ba0decae9f83b530d72e5d2ef12b
aleidy.villasmil@tecso.coop <aleidy.villasmil@tecso.coop>
2026-08-29 18:48:11 -0300

    Reto 4 completado

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -15,10 +15,10 @@ test('Check left menu options', async ({ page }) => {
 
     const currentMenuItems: string[] = []
 
-    for(let i = 0; i < currentMenuItemsCount; i++) {
+    for (let i = 0; i < currentMenuItemsCount; i++) {
         const menuText = await leftMenuItems.nth(i).innerText();
         currentMenuItems.push(menuText)
-        
+
     }
 
     console.log(currentMenuItems)
@@ -43,4 +43,32 @@ test('Check left menu options', async ({ page }) => {
     console.log('Primer menu item:', currentMenuItems[0]);
 
 
+});
+
+test('Navegando el panel izquierdo', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+
+    const leftMenuItems = await page.getByLabel('Sidepanel').getByRole('listitem')
+    const currentMenuItemsCount = await leftMenuItems.count()
+
+    for (let i = 0; i < currentMenuItemsCount; i++) {
+        const menuItem = await leftMenuItems.nth(i);
+        const menuText = await menuItem.innerText();
+
+
+        if (menuText === 'Maintenance') {
+            await menuItem.click();
+            await page.goBack({ waitUntil: 'commit' });
+        } else {
+            await menuItem.click();
+        }
+
+        console.log('Click en Menu item:', menuText);
+
+    }
 });
